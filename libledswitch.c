384a47eca35bd8b192ff38de21fac238f74c4e0e
Michael Hornung <mhornung.linux@gmail.com>
2015-03-09 22:56:09 +0100

    libledswitch.c: Include libledswitch header file

--- libledswitch.c
+++ libledswitch.c
@@ -14,7 +14,7 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-#include "librsswitch.h"
+#include "libledswitch.h"
 
 /**
  * Send a code word
